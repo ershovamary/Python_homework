0bc146417416aed8c7eee3ea6c557bca1d5dcf63
ershovamary <31881859+ershovamary@users.noreply.github.com>
2017-12-12 12:52:56 +0300

    Test: part 2

--- test/Test.py
+++ test/Test.py
@@ -1,11 +1,19 @@
 ## Test from 12 December 2017
 ## Вариант 1
 
-##Часть 1
+## Часть 1
 
 with open("Extinct_languages.tsv", "r", encoding="utf-8") as f:
     s = f.readlines()
+    crit = []
     for i in s:
         if len(i) > 35:
             print(i)
             
+## Часть 2
+            
+        columns = i.split('\t')
+        status = columns[2]
+        if status == "Critically endangered\n":
+            crit.append(status)
+    print(len(crit))
