88b6730be5d2185137148bebce550b966e983652
ershovamary <31881859+ershovamary@users.noreply.github.com>
2018-01-24 21:15:18 +0300

    changing some details in hw6
    deleting amount of files and adding one instead of all

--- hw6/homework6.py
+++ hw6/homework6.py
@@ -1,9 +1,29 @@
 ##Вариант 7
 ##Программа генерирует тёка -- набор из 4 танка,
 ##которые в свою очередь являются нерифмованными пятью строками в 5-7-5-7-7 слогов,
-##беря случайные слова из текстовых файлов.
+##беря случайные слова из текстового файла.
 
 import random
+
+def make_dictionary():
+    with open('dictionary.txt', encoding='utf-8') as f:
+        dictionary = dict(
+        noun_nom_m = f.readline().split(),
+        noun_nom_f = f.readline().split(),
+        noun_nom_n = f.readline().split(),
+        noun_gen = f.readline().split(),
+        noun_inst = f.readline().split(),
+        noun_loc = f.readline().split(),
+        clit_inst = f.readline().split(),
+        clit_gen = f.readline().split(),
+        clit_loc = f.readline().split(),
+        adj = f.readline().split(),
+        adv = f.readline().split(),
+        verb = f.readline().split(),
+        question_word = f.readline().split(),
+        pronoun = f.readline().split(),
+        imperative = f.readline().split())
+    return dictionary
 
 def punct():
 
@@ -11,38 +31,54 @@ def punct():
 
     return random.choice(marks)
 
-def list_from_file(a):
-    a += '.txt'
-    with open(a, encoding='utf-8') as f:
-        b = f.readline().split()
-    return b
-
 def noun_nom():
-    nouns_nom = list_from_file('noun_nom')
+    d =  make_dictionary()
+    nouns_nom = d['noun_nom_m'] + d['noun_nom_f'] + d['noun_nom_n']
+    return random.choice(nouns_nom)
+
+def noun_nom_m():
+    d =  make_dictionary()
+    nouns_nom = d['noun_nom_m']
+    return random.choice(nouns_nom)
+
+def noun_nom_f():
+    d =  make_dictionary()
+    nouns_nom = d['noun_nom_f']
+    return random.choice(nouns_nom)
+
+def noun_nom_n():
+    d =  make_dictionary()
+    nouns_nom = d['noun_nom_n']
     return random.choice(nouns_nom)
 
 def noun_gen():
-    nouns_gen = list_from_file('noun_gen')
+    d =  make_dictionary()
+    nouns_gen = d['noun_gen']
     return random.choice(nouns_gen)
 
 def noun_inst():
-    nouns_inst = list_from_file('noun_inst')
+    d =  make_dictionary()
+    nouns_inst = d['noun_inst']
     return random.choice(nouns_inst)
 
 def noun_loc():
-    nouns_loc = list_from_file('noun_loc')
+    d =  make_dictionary()
+    nouns_loc = d['noun_loc']
     return random.choice(nouns_loc)
 
 def clit_inst():
-    clitics = list_from_file('clit_inst')
+    d =  make_dictionary()
+    clitics = d['clit_inst']
     return random.choice(clitics)
 
 def clit_gen():
-    clitics = list_from_file('clit_gen')
+    d =  make_dictionary()
+    clitics = d['clit_gen']
     return random.choice(clitics)
 
 def clit_loc():
-    clitics = list_from_file('clit_loc')
+    d =  make_dictionary()
+    clitics = d['clit_loc']
     return random.choice(clitics)
 
 def adverbial():
@@ -53,34 +89,48 @@ def adverbial():
     return clit_loc() + ' ' + noun_loc() + ' ' +  noun_gen()
 
 def adj():
-    adjs = list_from_file('adj')
+    d =  make_dictionary()
+    adjs = d['adj']
     return random.choice(adjs)
 
 def adv():
-    advs = list_from_file('adv')
+    d =  make_dictionary()
+    advs = d['adv']
     return random.choice(advs)
 
 def verb():
-    verbs = list_from_file('verb')
+    d =  make_dictionary()
+    verbs = d['verb']
     return random.choice(verbs)
 
 def question_word():
-    question_words = list_from_file('question_word')
+    d =  make_dictionary()
+    question_words = d['question_word']
     return random.choice(question_words)
 
 def pronoun():
-    pronouns = list_from_file('pronoun')
+    d =  make_dictionary()
+    pronouns = d['pronoun']
     return random.choice(pronouns)
 
 def imperative():
-    imperatives = list_from_file('imperative')
+    d =  make_dictionary()
+    imperatives = d['imperative']
     return random.choice(imperatives)
+
+def noun_group():
+    r = random.randint(0,2)
+    if r == 0:
+        return adj() + 'ый ' + noun_nom_m()
+    elif r == 1:
+        return adj() + 'ая ' + noun_nom_f()
+    return adj() + 'ое ' + noun_nom_n()
 
 def long_sentence(r):
     a = adverbial()
     a = a.capitalize()
     if r % 2 == 0:
-        return str(a) + '\r\n' + adj().capitalize() + ' ' + noun_nom() + ' ' + verb() + punct() + '\r\n'
+        return str(a) + '\r\n' + noun_group() + ' ' + verb() + punct() + '\r\n'
     return str(a) + '\r\n' + noun_nom().capitalize() + ' ' + adv() + ' ' + verb() + punct() + '\r\n'
 
 def short_sentence(r):
